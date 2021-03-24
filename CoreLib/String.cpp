b092b88f7254a2e34a17561e757a00e7046e8fea
JomiXedYu <jomixedyu@outlook.com>
2021-03-24 23:13:53 +0800

    update string

---
+++ CoreLib/String.cpp
@@ -0,0 +1,26 @@
+#include "String.h"
+
+RefString operator+(const RefString& left, const RefString& right)
+{
+    return NewString(*left + *right);
+}
+
+RefString operator+(const StringPointer& left, const RefString& right)
+{
+    return NewString(left + *right);
+}
+
+RefString operator+(const RefString& left, const StringPointer& right)
+{
+    return NewString(*left + right);
+}
+
+RefString operator+(const RefString& left, const String& right)
+{
+    return NewString(*left + right);
+}
+
+RefString operator+(const String& left, const RefString& right)
+{
+    return NewString(left + *right);
+}
--- CoreLib/String.h
+++ CoreLib/String.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <memory>
 
+using StringPointer = const wchar_t*;
 using String = std::wstring;
 using RefString = std::shared_ptr<std::wstring>;
 using StringStream = std::wstringstream;
@@ -11,6 +12,14 @@ using StringStream = std::wstringstream;
 #define _T(str) L##str
 #define STR(str) String(L##str)
 
+
+RefString operator+(const RefString& left, const RefString& right);
+
+RefString operator+(const StringPointer& left, const RefString& right);
+RefString operator+(const RefString& left, const StringPointer& right);
+
+RefString operator+(const RefString& left, const String& right);
+RefString operator+(const String& left, const RefString& right);
 
 template<typename... Param>
 RefString NewString(Param&& ...str) {
