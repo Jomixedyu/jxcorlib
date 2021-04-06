26b15092d5e595dff9a64a930c43cc3c51d0463f
JomiXedYu <jomixedyu@outlook.com>
2021-04-06 21:33:26 +0800

    upload

--- JxCode.CoreLib.cpp
+++ JxCode.CoreLib.cpp
@@ -1,21 +1,50 @@
 ﻿#include <string>
 #include <iostream>
 #include "CoreLib/Object.h"
+#include <sstream>
 
 using namespace std;
 
 class TestClass : public Object
 {
-    DEF_OBJECT_TYPE(TestClass, Object)
+    DEF_OBJECT_TYPE(TestClass, Object);
+    DEF_OBJECT_CINSTCTOR(TestClass);
+private:
+    int id;
 public:
-    virtual RefString to_string() const {
+    TestClass(int _id) : id(_id) { }
+    DECL_TOSTRING() {
         return _T("Str: ") + this->get_type()->get_name();
+    }
+    DECL_EQUALS() {
+        return this->id == ((TestClass)target).id;
     }
 };
 
+class MemoryBlock
+{
+    int chunk_size;
+    void* head;
+    MemoryBlock* next;
+};
+class MemoryPool
+{
+    //8 16 24 32 48 64 72 80 88 96 104 112 120 128
+    MemoryBlock blocks[14];
+    MemoryPool()
+    {
+        for (int i = 0; i < 14; i++)
+        {
+            blocks[i] = MemoryBlock();
+        }
+    }
+};
+
+
 int main()
 {
-    TestClass* tc0 = new TestClass;
+
+    TestClass* tc0 = new TestClass(3);
 
     Type* type = tc0->get_type();
     wcout << *type->get_name() << endl;
@@ -24,6 +53,8 @@ int main()
     //如果没有生成（没调用过get_type），需要用typeof<TestClass>()或者RegisterClass<TestClass>()来前向注册。
     Object* tc = Type::GetType(_T("TestClass"))->CreateInstance();
     wcout << *tc->to_string() << endl;
+    
+
 
 }
 
