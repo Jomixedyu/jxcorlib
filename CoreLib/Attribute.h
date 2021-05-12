59bfd00b1d45b762fdbb3a05532ae4f51fe1a6fa
JomiXedYu <jomixedyu@outlook.com>
2021-05-12 20:03:19 +0800

    Property, Attribute

---
+++ CoreLib/Attribute.h
@@ -0,0 +1,27 @@
+#ifndef CORELIB_ATTRIBUTE_H
+#define CORELIB_ATTRIBUTE_H
+
+#include "OOPCore.h"
+
+enum class MemberType
+{
+    Class,
+    Field,
+    Method
+};
+
+template<typename T, MemberType memberType, typename Tag>
+struct $Attribute
+{
+
+};
+
+class Attribute : public Object
+{
+    DEF_OBJECT_TYPE(Attribute, Object);
+
+};
+
+#define $ATTRIBUTE(src, type, attribute) 
+
+#endif // !CORELIB_ATTRIBUTE_H
