53ccc71ea88c12b2a87755212ce1e6ec0ddbbbcf
JomiXedYu <jomixedyu@outlook.com>
2021-06-05 12:53:09 +0800

    string update

--- CoreLib/String.h
+++ CoreLib/String.h
@@ -20,21 +20,38 @@
 
 namespace JxCoreLib
 {
-    using String = std::string;
+    using string = std::string;
     using StringStream = std::stringstream;
 
     struct Char
     {
         char value[8]{ 0 };
-
-        static void Charcpy(char* dest, const char* src)
+        inline static int CharLen(const char* c)
         {
-            for (size_t i = 0; i < 8; i++) dest[0] = src[0];
+            for (int i = 0; i < 8; i++)
+            {
+                if (c[i] == 0) {
+                    return i;
+                }
+            }
+            return 8;
         }
-        static bool Charcmp(const char* l, const char* r)
+        inline static void Charcpy(char* dest, const char* src)
         {
-            for (size_t i = 0; i < 8; i++)
+            for (size_t i = 0; i < 8; i++) dest[i] = src[i];
+        }
+        inline static bool Charcmp(const char* l, const char* r)
+        {
+            int len = CharLen(l);
+            if (len != CharLen(r))
+            {
+                return false;
+            }
+            for (size_t i = 0; i < len; i++)
+            {
                 if (l[i] != r[i]) return false;
+            }
+
             return true;
         }
         Char() {}
@@ -50,26 +67,26 @@ namespace JxCoreLib
         {
             return !Charcmp(this->value, r.value);
         }
-        String ToString()
+        string ToString()
         {
-            return String(value);
+            return string(value);
         }
 
     };
 
-    bool operator==(const Char& left, const String& right);
-    bool operator==(const String& left, const Char& right);
-    bool operator!=(const Char& left, const String& right);
-    bool operator!=(const String& left, const Char& right);
-    String operator+(const Char& left, const String& right);
-    String operator+(const String& left, const Char& right);
+    bool operator==(const Char& left, const string& right);
+    bool operator==(const string& left, const Char& right);
+    bool operator!=(const Char& left, const string& right);
+    bool operator!=(const string& left, const Char& right);
+    string operator+(const Char& left, const string& right);
+    string operator+(const string& left, const Char& right);
 
     struct StringIndexMapping
     {
     private:
         size_t block_size_;
     public:
-        StringIndexMapping(const String& str, size_t block_size) noexcept(false);
+        StringIndexMapping(const string& str, size_t block_size) noexcept(false);
     public:
         std::vector<int> mapping;
         size_t get_block_size() const;
@@ -85,17 +102,17 @@ namespace JxCoreLib
         StringUtil() = delete;
     public:
         static bool IsLittleEndian() noexcept;
-        static size_t CharLen(const String& str, const size_t& pos) noexcept(false);
-        static String Replace(const String& src, const String& find, const String& target);
-        static Char PosAt(const String& src, const size_t& bytepos);
-        static Char CharAt(const String& src, const size_t& charpos);
-        static Char CharAt(const String& src, const size_t& charpos, const StringIndexMapping& mapping);
-        static size_t Length(const String& src);
-        static size_t Length(const String& src, const StringIndexMapping& mapping);
-        static std::vector<uint8_t> GetBytes(const String& str);
+        static size_t CharLen(const string& str, const size_t& pos) noexcept(false);
+        static string Replace(const string& src, const string& find, const string& target);
+        static Char PosAt(const string& src, const size_t& bytepos);
+        static Char CharAt(const string& src, const size_t& charpos);
+        static Char CharAt(const string& src, const size_t& charpos, const StringIndexMapping& mapping);
+        static size_t Length(const string& src);
+        static size_t Length(const string& src, const StringIndexMapping& mapping);
+        static std::vector<uint8_t> GetBytes(const string& str);
 
-        static std::u16string Utf8ToUtf16(const String& str);
-        static String Utf16ToUtf8(const std::u16string& str);
+        static std::u16string Utf8ToUtf16(const string& str);
+        static string Utf16ToUtf8(const std::u16string& str);
     };
 
 }
