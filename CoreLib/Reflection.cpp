68bee30cb4bb327d1ffd8b72258f389ca2192af8
JomiXedYu <JomiXedYu@outlook.com>
2021-06-10 20:36:21 +0800

    notimpl

--- CoreLib/Reflection.cpp
+++ CoreLib/Reflection.cpp
@@ -1,1 +1,39 @@
 #include "Reflection.h"
+
+
+namespace JxCoreLib
+{
+
+
+
+    Type* JxCoreLib::FieldInfo::GetFieldType() const
+    {
+        return nullptr;
+    }
+
+    void FieldInfo::SetValue(const string& name, const std::any& value)
+    {
+    }
+
+    std::any FieldInfo::GetValue(const string& name) const
+    {
+        return std::any();
+    }
+
+
+    std::vector<Type*> MethodInfo::GetParametersType() const
+    {
+        return std::vector<Type*>();
+    }
+
+    Type* MethodInfo::GetReturnType() const
+    {
+        return nullptr;
+    }
+
+    std::any MethodInfo::Invoke(const ParameterPackage& params) const
+    {
+        return std::any();
+    }
+
+}
