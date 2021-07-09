89ce04973dd1c24bf82d0600d32b5f0dfe765271
雪千渔 <JomiXedYu@outlook.com>
2021-07-10 02:05:57 +0800

    Update ManagedVector.h

--- CoreLib/ManagedVector.h
+++ CoreLib/ManagedVector.h
@@ -10,16 +10,46 @@ namespace JxCoreLib
     template<baseof_object_pointer_concept T>
     class ManagedVector : public Object, public std::vector<T>
     {
+        //TODO: Template Macro
     public:
         using type = std::vector<T>;
         using type::type;
 
-        virtual ~ManagedVector()
+        T take_at(const size_t& pos)
         {
-            for (auto& item : *this)
+            T t = this->at(pos);
+            this->erase(this->begin() + pos);
+            return t;
+        }
+        void remove_at_and_delete(const size_t& pos)
+        {
+            auto it = (this->begin() + pos);
+            delete (*it);
+            this->erase(it);
+        }
+        void remove_and_delete(const T& value)
+        {
+            auto it = std::find(this->begin(), this->end(), value);
+            delete (*it);
+            this->erase(it);
+        }
+        void clear_and_delete()
+        {
+            this->delete_all();
+            this->clear();
+        }
+    private:
+        void delete_all()
+        {
+            for (const auto& item : *this)
             {
                 delete item;
             }
+        }
+    public:
+        virtual ~ManagedVector()
+        {
+            this->delete_all();
         }
     };
 
