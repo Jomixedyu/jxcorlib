8c5d766f9ffa6f0fb5fbd92ec7f7ef1723a50925
雪千渔 <JomiXedYu@outlook.com>
2021-08-26 21:01:11 +0800

    Update DebugTool.h

--- CoreLib/DebugTool.h
+++ CoreLib/DebugTool.h
@@ -9,8 +9,22 @@
 #ifndef _CORELIB_DEBUGTOOL
 #define _CORELIB_DEBUGTOOL
 
+#include <string>
+
 #define DEBUG_INFO(info) std::format("info: {}; line: {}, file: {};", info, __LINE__, __FILE__)
 
 #define CHECK_NULLPOINTER(ptr) if(ptr == nullptr) throw JxCoreLib::NullPointerException(DEBUG_INFO(#ptr));
+
+template<typename T>
+T* AssertNull(T* v, const std::string& str)
+{
+    if (v == nullptr)
+    {
+        throw JxCoreLib::NullPointerException(str);
+    }
+    return v;
+}
+
+#define ANUL(ptr) AssertNull(ptr, DEBUG_INFO(#ptr))
 
 #endif // !_CORELIB_DEBUGTOOL
