561f70cf3d7e2ee06a2edabd11b8d04d43e88834
雪千渔 <JomiXedYu@outlook.com>
2022-07-28 16:26:53 +0800

    Update time_counter.h

--- CoreLib/time_counter.h
+++ CoreLib/time_counter.h
@@ -1,25 +1,20 @@
 #pragma once
 
 #include <ctime>
+
 struct time_counter_t
 {
 private:
 	clock_t start_time_ = 0;
 	clock_t count_ = 0;
 public:
-	void start()
-	{
-		this->start_time_ = ::clock();
-	}
-	clock_t end()
-	{
-		this->count_ = current();
-		return this->count_;
-	}
-	clock_t current() const
-	{
-		return ::clock() - this->start_time_;
-	}
+	void start() { this->start_time_ = ::clock(); }
+
+	clock_t end() { return this->count_ = current(); }
+
+	clock_t current() const { return ::clock() - this->start_time_; }
+	float current_sec() const { return current() / 1000.f; }
 
 	clock_t time() const { return this->count_; }
+	float time_sec() const { return this->count_ / 1000.f; }
 };
