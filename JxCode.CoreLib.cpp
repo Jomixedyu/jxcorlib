634e58838a9e05cf43f17f2f3f4c85ed9414ca84
雪千渔 <JomiXedYu@outlook.com>
2022-08-08 18:09:15 +0800

    Update JxCode.CoreLib.cpp

--- JxCode.CoreLib.cpp
+++ JxCode.CoreLib.cpp
@@ -23,49 +23,12 @@ extern void TestFile();
 
 
 
-#define CORELIB_DEF_ENUM(Assembly, FullName, Name, ...) \
-enum class Name : uint32_t \
-{ \
- __VA_ARGS__ \
-}; \
-inline struct __corelib_enum_##Assembly##_##FullName { \
-__corelib_enum_##Assembly##_##FullName() { ::JxCoreLib::Enum::StaticRegisterEnum(#__VA_ARGS__); } \
-} __corelib_enum_##Assembly##_##FullName##_; \
-template<> struct get_boxing_type<Mode> { using type = ::JxCoreLib::Enum; };
-
-
-
-enum class Mode { A = 1, B = 2 };
-
-class BoxingMode final : public Enum
-{
-    CORELIB_DEF_TYPE(AssemblyObject_JxCoreLib, BoxingMode, Enum);
-
-    static inline DataMap* definitions = nullptr;
-    static inline const char* string_definitions = "A = 1, B = 2";
-    static void init_defs() { InitDefinitions(string_definitions, &definitions); }
-public:
-    using unboxing_type = Mode;
-    static const DataMap* StaticGetDefinitions() {
-        init_defs();
-        return definitions;
-    }
-    static string StaticFindName(Mode value) {
-        init_defs();
-        for (auto& [name, enum_value] : *definitions) {
-            if (enum_value == static_cast<uint32_t>(value)) return name;
-        }
-        return {};
-    }
-    string GetName() const override {
-        return StaticFindName(static_cast<Mode>(this->value_));
-    }
-    BoxingMode& operator=(Mode value) {
-        this->value_ = static_cast<uint32_t>(value);
-    }
-    BoxingMode(Mode value) : base(static_cast<uint32_t>(value)) { }
-    BoxingMode() : base() {  }
-};
+CORELIB_DEF_ENUM(AssemblyObject_JxCoreLib,
+    , Mode,
+    A = 1,
+    B = 2,
+    C = 3
+    );
 
 int main()
 {
@@ -83,6 +46,8 @@ int main()
 
     BoxingMode m = Mode::B;
     cout << m.GetName() << endl;;
+    auto f = cltypeof<BoxingMode>()->GetEnumDefinitions();
+    cout << cltypeof<BoxingMode>()->get_name() <<endl;
 
     sptr<List<String_sp>> intlist = mksptr(new List<String_sp>);
     //interface
