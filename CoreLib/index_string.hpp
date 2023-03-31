341097e1e3a861698ae02524273aac7631e1cafe
雪千渔 <30803536+JomiXedYu@users.noreply.github.com>
2023-03-31 18:16:59 +0800

    Update index_string.hpp

--- CoreLib/index_string.hpp
+++ CoreLib/index_string.hpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2021 JomiXedYu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #pragma once
 #include <unordered_map>
 #include <string>
@@ -21,13 +45,14 @@ struct __index_string_manager final
     }
 };
 
-template<typename char_t>
-struct basic_index_string final
+template<typename T>
+struct basic_index_string
 {
-private:
+protected:
+    using char_t = T;
     using index_t = size_t;
     constexpr static index_t none = 0;
-private:
+protected:
     static size_t ELFHash(const char_t* str, size_t size)
     {
         static const size_t TotalBits = sizeof(size_t) * 8;
@@ -89,7 +114,7 @@ public:
                 if (str_len == view.length() && ::memcmp(it->second.bytes, view.data(), str_len * sizeof(char_t)))
                 {
                     collision = true;
-                    hash = ELFHash(hash);
+                    ++hash;
                 }
 
             } while (collision);
