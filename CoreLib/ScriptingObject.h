f125771571e81f4169c11fa67fc572bccefb7fea
雪千渔 <JomiXedYu@outlook.com>
2023-05-06 11:07:37 +0800

    scripting object

---
+++ CoreLib/ScriptingObject.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Object.h"
+#include "Type.h"
+
+namespace jxcorlib
+{
+    class ScriptingObject : public Object
+    {
+        CORELIB_DEF_TYPE(AssemblyObject_jxcorlib, jxcorlib::ScriptingObject, Object);
+    public:
+        
+    }
+}
